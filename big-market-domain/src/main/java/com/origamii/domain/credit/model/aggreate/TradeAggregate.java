947425c12a0330b4bfd2b83f03c813f65ce5c5a5
TobiichiOrigamii <origami9103@gmail.com>
2024-11-09 00:42:59 +0800

    营销服务 第31节：分库分表数据同步ES bug2: 在TradeAggregate的createCreditOrderEntity方法中，缺少orderId的赋值 完成

--- big-market-domain/src/main/java/com/origamii/domain/credit/model/aggreate/TradeAggregate.java
+++ big-market-domain/src/main/java/com/origamii/domain/credit/model/aggreate/TradeAggregate.java
@@ -12,6 +12,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.apache.commons.lang3.RandomStringUtils;
 
 import java.math.BigDecimal;
 
@@ -49,6 +50,7 @@ public class TradeAggregate {
     public static CreditOrderEntity createCreditOrderEntity(String userId, TradeNameVO tradeName, TradeTypeVO tradeType, BigDecimal tradeAmount, String outBusinessNo) {
         return CreditOrderEntity.builder()
                 .userId(userId)
+                .orderId(RandomStringUtils.randomNumeric(12))
                 .tradeName(tradeName)
                 .tradeType(tradeType)
                 .tradeAmount(tradeAmount)
